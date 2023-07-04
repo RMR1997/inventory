56d009eadedf986e3836dda475f448b3cc9d4972
RMR1997 <maulanarizkhie@gmail.com>
2023-07-04 10:43:22 +0700

    Update Controllers

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,128 +1,129 @@
-const { admin,category,item,location,ownership } = require("../../models");
+const { admin, category, item, location, ownership } = require("../../models");
 const joi = require("joi");
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 
 
-exports.login = async (req,res) =>{
-    try{
-        const body = req.body
+exports.login = async (req, res) => {
 
-        const schema = joi.object({
-            email: joi.string().email().required(),
-            password: joi.string().min(8).required()
-        })
+  try {
+    const body = req.body
 
-        const { error } = schema.validate(body)
+    const schema = joi.object({
+      email: joi.string().email().required(),
+      password: joi.string().min(8).required()
+    })
 
-        if(error){
-            console.log(error)
-            return res.status(400).send({
-                message: error.details[0].message
-            })
-        }
+    const { error } = schema.validate(body)
 
-        let dataLogin = await admin.findOne({
-            where: {
-                email: req.body.email
-            }
-        })
-
-        if(!dataLogin){
-            return res.status(400).send({
-                message: 'Email doesnt match'
-            })
-        }
-        const match = await bcrypt.compare(req.body.password, dataLogin.password)
-        if(!match){
-            return res.status(400).send({
-                message: ' password doesnt match'
-            })
-        }
-
-        let dataloginid = await admin.findOne({
-            where: {
-                email: req.body.email
-            },
-            attributes: {
-                exclude: ["createdAt", "updatedAt", "password"]
-            }
-        })
-
-        const accessToken = jwt.sign(
-            {id: dataloginid.id},
-        process.env.ACCESS_TOKEN_SECRET
-        )
-
-        res.status(200).send({
-            message: 'berhasil login', 
-            data: dataloginid,
-            token: accessToken
-        })
-    }catch (err){
-        console.error(err)
+    if (error) {
+      console.log(error)
+      return res.status(400).send({
+        message: error.details[0].message
+      })
     }
+
+    let dataLogin = await admin.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+
+    if (!dataLogin) {
+      return res.status(400).send({
+        message: 'Email doesnt match'
+      })
+    }
+    const match = await bcrypt.compare(req.body.password, dataLogin.password)
+    if (!match) {
+      return res.status(400).send({
+        message: ' password doesnt match'
+      })
+    }
+
+    let dataloginid = await admin.findOne({
+      where: {
+        email: req.body.email
+      },
+      attributes: {
+        exclude: ["createdAt", "updatedAt", "password"]
+      }
+    })
+
+    const accessToken = jwt.sign(
+      { id: dataloginid.id },
+      process.env.ACCESS_TOKEN_SECRET
+    )
+
+    res.status(200).send({
+      message: 'berhasil login',
+      data: dataloginid,
+      token: accessToken
+    })
+  } catch (err) {
+    console.error(err)
+  }
 }
 
-exports.Register = async(req, res) => {
-    try {
+exports.Register = async (req, res) => {
+  try {
 
-        const data = req.body
-        const schema = joi.object({
-            email: joi.string().email().required(),
-            password: joi.string().min(8).required(),
-          
-        })
-       
-        const {error} = schema.validate(data)
-       
-        
-        if(error){
-            
-            console.log(error.details)
-            return res.status(400).send({
-                message: error.details[0].message
-            })
-        }
-        
-        const hash = await bcrypt.hash(req.body.password, 10)
+    const data = req.body
+    const schema = joi.object({
+      email: joi.string().email().required(),
+      password: joi.string().min(8).required(),
 
-        const email = await admin.findOne({
-            where: {
-                email:req.body.email 
-            }
-        })
+    })
 
-        if(email) {
-            return res.status(400).send({
-                message: `email sudah di pakai`
-            })
-        }
-        
-        const save = await admin.create({
-           
-            email: req.body.email,
-            password: hash,
-            
-        })
-        
+    const { error } = schema.validate(data)
 
-        const simpan = await admin.findOne({
-            where : {id:save.id},
-            attributes: {
-                exclude : ["password"]
-            }
-        })
-        console.log(simpan);
-      
-        res.send({
-            status: 200,
-            message: 'berhasil simpan',
-            data: simpan
-        })
-    } catch (err) {
-        console.error(err);
+
+    if (error) {
+
+      console.log(error.details)
+      return res.status(400).send({
+        message: error.details[0].message
+      })
     }
+
+    const hash = await bcrypt.hash(req.body.password, 10)
+
+    const email = await admin.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+
+    if (email) {
+      return res.status(400).send({
+        message: `email sudah di pakai`
+      })
+    }
+
+    const save = await admin.create({
+
+      email: req.body.email,
+      password: hash,
+
+    })
+
+
+    const simpan = await admin.findOne({
+      where: { id: save.id },
+      attributes: {
+        exclude: ["password"]
+      }
+    })
+    console.log(simpan);
+
+    res.send({
+      status: 200,
+      message: 'berhasil simpan',
+      data: simpan
+    })
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 exports.getAllItems = async (req, res) => {
@@ -142,10 +143,10 @@ exports.getAllItems = async (req, res) => {
           },
         },
         {
-            model: ownership,
-            attributes: {
-                exclude: ["createdAt", "updatedAt"],
-              },
+          model: ownership,
+          attributes: {
+            exclude: ["createdAt", "updatedAt"],
+          },
         },
       ],
       attributes: {
@@ -153,8 +154,9 @@ exports.getAllItems = async (req, res) => {
       },
     });
 
-    
-    return res.json({ item: datas});
+
+
+    return res.json({ item: datas });
 
   } catch (error) {
     console.log(error);
@@ -165,269 +167,276 @@ exports.getAllItems = async (req, res) => {
 };
 
 exports.getItemById = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const data = [];
-    
-        if (data[0] == null) {
-          const userData = await item.findOne({
-            where: {
-              id,
-            },
-            attributes: {
-              exclude: ["createdAt", "updatedAt"],
-            },
-            include: [
-                {
-                  model: category,
-                  attributes: {
-                    exclude: ["createdAt", "updatedAt"],
-                  },
-                },
-                {
-                  model: location,
-                  attributes: {
-                    exclude: ["createdAt", "updatedAt"],
-                  },
-                },
-                {
-                    model: ownership,
-                    attributes: {
-                        exclude: ["createdAt", "updatedAt"],
-                      },
-                },
-              ],
-          });
-    
-          if (userData == undefined || userData == null) {
-            return res.status(400).send({
-              message: `id item ${id} not Found`,
-            });
-          }
-    
-          console.log("USER DATA: ", userData);
-      return res.status(200).send({
-            message: `data item ${id}`,
-            data: userData,
-          });
-        }
-      } catch (error) {
-        
-        console.log(error);
-        return res.send({
-          status: 500,
-          message: "Internal server error!",
-        });
-      }
-    };
+  try {
+    const id = req.params.id;
+    const data = [];
 
-    exports.AddItem = async (req, res) => {
-        try {
-          const body = req.body;
-
-          const schema = joi.object({
-            itemId: joi.string().min(4).required(),
-            itemName: joi.string().min(3).required(),
-            categoryId: joi.number().min(1).required(),
-            ownershipId: joi.number().min(1).required(),
-            locationId: joi.number().min(1).required(),
-            qty: joi.number().min(1).required(),
-            status: joi.number().min(1).required(),
-            purchaseDate: joi.date().required()
-          });
-      
-          const { error } = schema.validate(body);
-      
-          if (error) {
-            return res.status(400).send({
-              message: error.details[0].message,
-            });
-          }
-          
-        
-          const itemId = await item.findOne({
-            where: {
-                itemId:body.itemId
-            }
-        })
-
-        if(itemId) {
-            return res.status(400).send({
-                message: `Item Id sudah ada`
-            })
-        }
-
-         
-
-          const requestItem = {
-            itemId: body.itemId,
-            itemName: body.itemName,
-            categoryId: body.categoryId,
-            ownershipId: body.ownershipId,
-            locationId: body.locationId,
-            qty: body.qty,
-            status: body.status,
-            purchaseDate: body.purchaseDate,
-          };
-      
-          const data = await item.create(requestItem);
-      
-        res.json({item:data})
-        } catch (error) {
-          console.log(error);
-          return res.status(500).send({
-            message: "Internal server error!",
-          });
-        }
-      };
-
-      exports.getCategory = async (req, res) => {
-        try {
-          const datas = await category.findAll({
-            attributes: {
-              exclude: ["createdAt", "updatedAt"],
-            },
-          });
-      
-          
-          return res.json({ item: datas});
-      
-        } catch (error) {
-          console.log(error);
-          return res.status(500).send({
-            message: "Internal server error",
-          });
-        }
-      };
-
-
-      exports.getOwnership = async (req, res) => {
-        try {
-          const datas = await ownership.findAll({
-            attributes: {
-              exclude: ["createdAt", "updatedAt"],
-            },
-          });
-      
-          
-          return res.json({ item: datas});
-      
-        } catch (error) {
-          console.log(error);
-          return res.status(500).send({
-            message: "Internal server error",
-          });
-        }
-      };
-
-      exports.getLocation = async (req, res) => {
-        try {
-          const datas = await location.findAll({
-            attributes: {
-              exclude: ["createdAt", "updatedAt"],
-            },
-          });
-      
-          
-          return res.json({ item: datas});
-      
-        } catch (error) {
-          console.log(error);
-          return res.status(500).send({
-            message: "Internal server error",
-          });
-        }
-      };
-
-
-      exports.editItem = async (req, res) => {
-        try {
-        const id = req.params.id;
-          const body = req.body;
-
-          const schema = joi.object({
-            itemId: joi.string().min(4).required(),
-            itemName: joi.string().min(3).required(),
-            categoryId: joi.number().min(1).required(),
-            ownershipId: joi.number().min(1).required(),
-            locationId: joi.number().min(1).required(),
-            qty: joi.number().min(1).required(),
-            status: joi.number().min(1).required(),
-            purchaseDate: joi.date().required()
-          });
-      
-          const { error } = schema.validate(body);
-      
-          if (error) {
-            return res.status(400).send({
-              message: error.details[0].message,
-            });
-          }
-          const findItem = await item.findOne({
-            where: {
-                id : id
-            }
-          })
-
-          if(!findItem){
-            return res.status(404).send({
-                message: 'data tidak ditemukan'
-            })
-        }
-
-        // const findOwnershipCode = ownership.findOne({
-        //   where: {body.}
-        // })
-
-          const requestItem = {
-            itemId: body.itemId,
-            itemName: body.itemName,
-            categoryId: body.categoryId,
-            ownershipId: body.ownershipId,
-            locationId: body.locationId,
-            qty: body.qty,
-            status: body.status,
-            purchaseDate: body.purchaseDate,
-          };
-      
-          await findItem.update(requestItem,{
-            where : {
-                id
-            }
-          });
-        
-      
-        return res.status(200).send({
-          message:"Data Updated",
-          item:findItem
-        })
-        
-        } catch (error) {
-          console.log(error);
-          return res.status(500).send({
-            message: "Internal server error!",
-          });
-        }
-      };
-
-exports.deleteItem = async (req, res) => {
-    try {
-      const id = req.params.id;
-
-      await item.destroy({
+    if (data[0] == null) {
+      const userData = await item.findOne({
         where: {
           id,
         },
+        attributes: {
+          exclude: ["createdAt", "updatedAt"],
+        },
+        include: [
+          {
+            model: category,
+            attributes: {
+              exclude: ["createdAt", "updatedAt"],
+            },
+          },
+          {
+            model: location,
+            attributes: {
+              exclude: ["createdAt", "updatedAt"],
+            },
+          },
+          {
+            model: ownership,
+            attributes: {
+              exclude: ["createdAt", "updatedAt"],
+            },
+          },
+        ],
       });
-  
+
+      if (userData == undefined || userData == null) {
+        return res.status(400).send({
+          message: `id item ${id} not Found`,
+        });
+      }
+
+      console.log("USER DATA: ", userData);
       return res.status(200).send({
-        message: "OK",
-      });
-  
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send({
-        message: "Internal server error!",
+        message: `data item ${id}`,
+        data: userData,
       });
     }
-  };
+  } catch (error) {
+
+    console.log(error);
+    return res.send({
+      status: 500,
+      message: "Internal server error!",
+    });
+  }
+};
+
+exports.AddItem = async (req, res) => {
+  try {
+    const body = req.body;
+
+    const schema = joi.object({
+      itemName: joi.string().min(3).required(),
+      categoryId: joi.number().min(1).required(),
+      ownershipId: joi.number().min(1).required(),
+      locationId: joi.number().min(1).required(),
+      qty: joi.number().min(1).required(),
+      status: joi.number().min(1).required(),
+      purchaseDate: joi.date().required(),
+    });
+
+    const { error } = schema.validate(body);
+
+    if (error) {
+      return res.status(400).send({
+        message: error.details[0].message,
+      });
+    }
+
+    const owner = await ownership.findOne({
+      where: {
+        id: body.ownershipId,
+      },
+    });
+
+    const code = owner.ownershipCode;
+
+    const requestItem = {
+      itemName: body.itemName,
+      categoryId: body.categoryId,
+      ownershipId: body.ownershipId,
+      locationId: body.locationId,
+      qty: body.qty,
+      status: body.status,
+      purchaseDate: body.purchaseDate,
+    };
+
+    const newItem = await item.create(requestItem);
+
+    const response = newItem.id.toString();
+
+    let itemId = response;
+
+    if (itemId.length === 1) {
+      itemId = "000" + itemId;
+    } else if (itemId.length === 2) {
+      itemId = "00" + itemId;
+    }
+
+    const ownershipCode = code + itemId;
+    newItem.itemId = ownershipCode;
+
+    await newItem.save();
+
+    res.status(200).send({
+      status: 200,
+      msg: `OK`,
+      data: newItem,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      message: "Internal server error!",
+    });
+  }
+};
+
+exports.getCategory = async (req, res) => {
+  try {
+    const datas = await category.findAll({
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+    });
+
+
+    return res.json({ item: datas });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      message: "Internal server error",
+    });
+  }
+};
+
+
+exports.getOwnership = async (req, res) => {
+  try {
+    const datas = await ownership.findAll({
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+    });
+
+
+    return res.json({ item: datas });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      message: "Internal server error",
+    });
+  }
+};
+
+exports.getLocation = async (req, res) => {
+  try {
+    const datas = await location.findAll({
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+    });
+
+
+    return res.json({ item: datas });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      message: "Internal server error",
+    });
+  }
+};
+
+
+exports.editItem = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const body = req.body;
+
+    const schema = joi.object({
+      itemId: joi.string().min(4).required(),
+      itemName: joi.string().min(3).required(),
+      categoryId: joi.number().min(1).required(),
+      ownershipId: joi.number().min(1).required(),
+      locationId: joi.number().min(1).required(),
+      qty: joi.number().min(1).required(),
+      status: joi.number().min(1).required(),
+      purchaseDate: joi.date().required()
+    });
+
+    const { error } = schema.validate(body);
+
+    if (error) {
+      return res.status(400).send({
+        message: error.details[0].message,
+      });
+    }
+    const findItem = await item.findOne({
+      where: {
+        id: id
+      }
+    })
+
+    if (!findItem) {
+      return res.status(404).send({
+        message: 'data tidak ditemukan'
+      })
+    }
+
+
+    const requestItem = {
+      itemId: body.itemId,
+      itemName: body.itemName,
+      categoryId: body.categoryId,
+      ownershipId: body.ownershipId,
+      locationId: body.locationId,
+      qty: body.qty,
+      status: body.status,
+      purchaseDate: body.purchaseDate,
+    };
+
+    await findItem.update(requestItem, {
+      where: {
+        id
+      }
+    });
+
+
+    return res.status(200).send({
+      message: "Data Updated",
+      item: findItem
+    })
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      message: "Internal server error!",
+    });
+  }
+};
+
+exports.deleteItem = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    await item.destroy({
+      where: {
+        id,
+      },
+    });
+
+    return res.status(200).send({
+      message: "OK",
+    });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      message: "Internal server error!",
+    });
+  }
+};
